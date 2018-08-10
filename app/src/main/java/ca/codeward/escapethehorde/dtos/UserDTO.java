43ed1db5606650e0cd15f720906e34564e4c5d33
Gorikie <david_ward_is@mail.com>
2018-08-09 22:59:58 -0400

    Base user DTO
    added user DTO

---
+++ app/src/main/java/ca/codeward/escapethehorde/dtos/UserDTO.java
@@ -0,0 +1,10 @@
+package ca.codeward.escapethehorde.dtos;
+
+import android.content.Context;
+
+import ca.codeward.escapethehorde.helper.DBAdapter;
+
+public class UserDTO extends DBAdapter {
+    public UserDTO(Context context) { super (context); }
+}
+
